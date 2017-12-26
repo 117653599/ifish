7aeb3c9c8108104be76001631bea67d15cad7c4f
linhuaqing <linhq2@wangsu.com>
2017-12-26 17:23:32 +0800

    修改模板名称

--- bin/ifish.js
+++ bin/ifish.js
@@ -4,7 +4,6 @@ const program = require('commander')
 
 program.version(require('../package.json').version)
   .usage('<command> [项目名称]')
-  .command('hello', '命令说明：启动hello')
   .command('init', '命令说明：创建新项目')
   .command('list', '命令说明：查看模板')
   .parse(process.argv)
